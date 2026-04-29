518702cee9f9ec7babccd4b36dcf61829d429248
unihykes <33852613@163.com>
2026-04-29 16:25:57 +0800

    按照行为拆分hook脚本

---
+++ hooks/r2e-hook-on-subagentStart.ps1
@@ -0,0 +1,13 @@
+param()
+
+. (Join-Path $PSScriptRoot "r2e-hook-common.ps1")
+
+# Hook: subagentStart
+Set-HookOutputUtf8
+$rawInput = Read-HookRawInput
+$logData = Get-HookLogData -RawInput $rawInput
+$projectDir = Get-HookProjectDir
+Write-HookLog -LogData $logData -ProjectDir $projectDir
+$null = Normalize-HookRawInputForDebug -RawInput $rawInput
+Write-HookAllowResponse
+exit 0
